a6f09d69c2d180365f1f2398b1162716d8928094
Joan <joan300501@gmail.com>
2023-04-19 09:28:42 -0500

    feat: Filter Search Clear button

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -3,19 +3,37 @@ import "animate.css";
 import { filterTags } from "./../data/filterTags.ts";
 import { filterTag } from "./../interfaces/interfaces";
 import { CloseCircleFilled } from "@ant-design/icons";
+import { useState, useRef } from "react";
+import type { InputRef } from "antd";
 
 function Filter() {
 	const deleteFilterTag = (filterTag: filterTag) => {
 		filterTags.filter((tag: filterTag) => tag.id !== filterTag.id);
 	};
+	const filterInputRef = useRef<InputRef>(null);
+	const deleteInputs = () => {
+		filterInputRef.current === null;
+	};
+	const [filterCleanerVisible, setFilterCleanerVisible] = useState(true);
 	return (
-		<div className="animate__animated animate__flipInX flex gap-4 mx-3 mb-3 justify-between items-center">
+		<div className=" flex gap-4 mx-3 mb-3 justify-between items-center">
 			<Select className="text-sm w-40" id="filters" placeholder="Filter by">
 				<Select.Option value="priority">Priority</Select.Option>
 				<Select.Option value="categories">Categories</Select.Option>
 				<Select.Option value="stakeholders">Stakeholders</Select.Option>
 			</Select>
-			<Input placeholder="tags" suffix={<CloseCircleFilled />} />
+			<Input
+				placeholder="tags"
+				ref={filterInputRef}
+				suffix={
+					filterCleanerVisible ? (
+						<CloseCircleFilled
+							onClick={deleteInputs}
+							className="cursor-pointer hover:text-blue-500 animate__animated animate__flipInX"
+						/>
+					) : null
+				}
+			/>
 		</div>
 	);
 }
